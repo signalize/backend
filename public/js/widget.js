a2f895bebc8ae519c4a6b5845cf98c6a75631134
Maikel ten Voorde <info@matevo.nl>
2019-02-16 20:31:35 +0100

    Initial Commit

--- public/js/widget.js
+++ public/js/widget.js
@@ -1,10 +1,23 @@
 export class Widget extends HTMLElement {
+    channel = null;
+
     constructor() {
         super();
-        this.innerHTML = this.render();
+        const widget = this;
+
+        if (this.channel) {
+            let socketAddress = 'ws://' + window.location.host + ':' + window.location.port + '/sock/';
+            socketAddress += '?SID=0FB1CF9AAB460F7834CB248B4DDFA1FDC9A804C7.1550220392';
+
+            const socket = new WebSocket(socketAddress);
+            socket.addEventListener('message', function (event) {
+                console.log(event.data);
+                widget.innerHTML = widget.render(event.data);
+            });
+        }
     }
 
-    render() {
+    render(data) {
         return '';
     }
 }
