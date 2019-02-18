96a5f1efb5efdc70eba6f23eb42c883b7d8d2068
Maikel ten Voorde <info@matevo.nl>
2019-02-18 16:59:50 +0100

    Initial Commit

--- src/Database.php
+++ src/Database.php
@@ -1,7 +1,42 @@
 <?php
 
 namespace Signalize;
-class Database extends \MongoLite\Client
+class Database
 {
+    /** @var string $database */
+    static $database;
 
+    /** @var \MongoLite\Database $database */
+    static $_databaseInstance;
+
+
+    static private function connect()
+    {
+        static::$_databaseInstance = (new \MongoLite\Client("databases"))->moduleP1;
+    }
+
+    /**
+     * @param $collection
+     * @param $data
+     * @return mixed
+     */
+    static public function store($collection, $data)
+    {
+        if (!static::$_databaseInstance) {
+            static::connect();
+        }
+        return static::$_databaseInstance->{$collection}->save($data);
+    }
+
+    /**
+     * @param string $collection
+     * @return \MongoLite\Collection|object
+     */
+    static public function collection($collection)
+    {
+        if (!static::$_databaseInstance) {
+            static::connect();
+        }
+        return static::$_databaseInstance->{$collection};
+    }
 }
